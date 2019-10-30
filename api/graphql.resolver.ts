605cf55c811ba37c86bee39b5cd1d69c4bdf3564
maclipi <mayankanildwivedi@gmail.com>
2019-10-30 15:25:40 +0530

    Changes

--- api/graphql.resolver.ts
+++ api/graphql.resolver.ts
@@ -25,7 +25,7 @@ const GQLresolver = async () => {
 
         }
         console.log(args.filters);
-        let url = `/youtube_donwloader/api/v1/downloadService?pageNumber=${args.pageNumber || 1}&limit=10`;
+        let url = `${process.env.API_END_POINT}/youtube_donwloader/api/v1/downloadService?pageNumber=${args.pageNumber || 1}&limit=10`;
         const mastermenu = await PostApiCall(url, args.filters);
         
         console.log(url);
